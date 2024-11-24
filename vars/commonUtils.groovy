aedacd81ce60d9643704e60cac9b2dce9fc39795
xiatiantian <1529450317@qq.com>
2024-11-24 16:21:35 +0800

    ..

---
+++ vars/commonUtils.groovy
@@ -0,0 +1,14 @@
+def buildApp() {
+    echo "Building the application..."
+    // 构建相关逻辑
+}
+
+def testApp() {
+    echo "Running tests on the application..."
+    // 测试相关逻辑
+}
+
+def deployApp(String env) {
+    echo "Deploying the application to ${env}..."
+    // 部署相关逻辑
+}
